b2aa1770a1e9c63020301dd40bc1175c510c2463
Peter Trost <p.trost93@gmail.com>
2025-04-16 22:31:06 +0200

    feat: add freezed dependency

--- riverpod_app/lib/domain/challenge.dart
+++ riverpod_app/lib/domain/challenge.dart
@@ -6,7 +6,16 @@ part 'challenge.freezed.dart';
 part 'challenge.g.dart';
 
 @freezed
+
+/// Represents a challenge with a [question], multiple [options], and correct
+/// [answer IDs]. The [correctAnswerIds] list contains the IDs of the [Answer]
+/// objects in the [options] list that are considered correct responses to the
+/// challenge.
 class Challenge with _$Challenge {
+  @Assert(
+    'options.map((o) => o.id).toSet().containsAll(correctAnswerIds)',
+    'Each correctAnswerId must match an id in options',
+  )
   const factory Challenge({
     required String id,
     required Localizations question,
