5f5f1e07d6bb507805be77cf32d2b1351b74f25e
Peter Trost <p.trost93@gmail.com>
2025-04-22 12:53:37 +0200

    test: add test to ensure the dio provider is used for the rest client

--- riverpod_app/test/data/services/rest_client_test.dart
+++ riverpod_app/test/data/services/rest_client_test.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:riverpod_app/app/config.dart';
 import 'package:riverpod_app/data/services/interceptors/json_interceptor.dart';
 import 'package:riverpod_app/data/services/rest_client.dart';
@@ -42,25 +43,39 @@ void main() {
   });
 
   group('restClientProvider', () {
-    late MockDio mockDio;
+    late MockDio dio;
 
     setUp(() {
-      mockDio = MockDio();
+      dio = MockDio();
     });
 
     RestClient getRestClient() {
       final container = ProviderContainer(
         overrides: [
-          dioProvider.overrideWithValue(mockDio),
+          dioProvider.overrideWithValue(dio),
         ],
       );
       addTearDown(container.dispose);
       return container.read(restClientProvider);
     }
 
-    test('should create RestClient instance', () {
+    test('should use the provided Dio instance for API calls', () async {
       final client = getRestClient();
-      expect(client, isA<RestClient>());
+
+      when(dio.options).thenReturn(BaseOptions());
+      when(dio.fetch<List<dynamic>>(any)).thenAnswer(
+        (_) async => Response(
+          requestOptions: RequestOptions(),
+          data: [],
+          statusCode: 200,
+        ),
+      );
+
+      await client.getConcepts();
+      verify(dio.fetch<List<dynamic>>(any)).called(1);
+
+      await client.getChallenges();
+      verify(dio.fetch<List<dynamic>>(any)).called(1);
     });
   });
 }
