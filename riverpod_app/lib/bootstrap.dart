91fbc210a84a126e0f208b5b135ddffde40805e9
Peter Trost <p.trost93@gmail.com>
2025-04-23 09:00:33 +0200

    feat: bootstrap Riverpod with ProviderScope and AppRiverpodObserver (Phase 1)

--- riverpod_app/lib/bootstrap.dart
+++ riverpod_app/lib/bootstrap.dart
@@ -2,11 +2,18 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+Future<void> bootstrap(FutureOr<Widget> Function() builder,
+    {List<ProviderObserver>? observers}) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  runApp(await builder());
+  runApp(
+    ProviderScope(
+      observers: observers ?? const [],
+      child: await builder(),
+    ),
+  );
 }
--- riverpod_app/lib/main_staging.dart
+++ riverpod_app/lib/main_staging.dart
@@ -1,6 +1,10 @@
 import 'package:riverpod_app/app/app.dart';
 import 'package:riverpod_app/bootstrap.dart';
+import 'package:riverpod_app/observers/app_riverpod_observer.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(
+    () => const App(),
+    observers: [AppRiverpodObserver()],
+  );
 }
