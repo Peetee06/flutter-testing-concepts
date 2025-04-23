34a438df793bc50a5d52fc511c236aa5e42f350d
Peter Trost <p.trost93@gmail.com>
2025-04-23 09:03:24 +0200

    fix: address linter warnings and formatting in AppRiverpodObserver

--- riverpod_app/lib/observers/app_riverpod_observer.dart
+++ riverpod_app/lib/observers/app_riverpod_observer.dart
@@ -4,30 +4,53 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AppRiverpodObserver extends ProviderObserver {
   @override
-  void didUpdateProvider(ProviderBase provider, Object? previousValue,
-      Object? newValue, ProviderContainer container) {
-    log('[Riverpod] ${provider.name ?? provider.runtimeType} updated: $previousValue -> $newValue');
-    super.didUpdateProvider(provider, previousValue, newValue, container);
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    log(
+      '[Riverpod] \\${provider.name ?? provider.runtimeType} updated: \\$previousValue -> \\$newValue',
+    );
+    super.didUpdateProvider(
+      provider,
+      previousValue,
+      newValue,
+      container,
+    );
   }
 
   @override
   void didAddProvider(
-      ProviderBase provider, Object? value, ProviderContainer container) {
-    log('[Riverpod] ${provider.name ?? provider.runtimeType} added: $value');
+    ProviderBase<Object?> provider,
+    Object? value,
+    ProviderContainer container,
+  ) {
+    log('[Riverpod] \\${provider.name ?? provider.runtimeType} added: \\$value');
     super.didAddProvider(provider, value, container);
   }
 
   @override
-  void didDisposeProvider(ProviderBase provider, ProviderContainer container) {
-    log('[Riverpod] ${provider.name ?? provider.runtimeType} disposed');
+  void didDisposeProvider(
+    ProviderBase<Object?> provider,
+    ProviderContainer container,
+  ) {
+    log('[Riverpod] \\${provider.name ?? provider.runtimeType} disposed');
     super.didDisposeProvider(provider, container);
   }
 
   @override
-  void providerDidFail(ProviderBase provider, Object error,
-      StackTrace stackTrace, ProviderContainer container) {
-    log('[Riverpod] ${provider.name ?? provider.runtimeType} error: $error',
-        stackTrace: stackTrace);
+  void providerDidFail(
+    ProviderBase<Object?> provider,
+    Object error,
+    StackTrace stackTrace,
+    ProviderContainer container,
+  ) {
+    log(
+      '[Riverpod] \\${provider.name ?? provider.runtimeType} error: \\$error',
+      stackTrace: stackTrace,
+    );
     super.providerDidFail(provider, error, stackTrace, container);
   }
 }
