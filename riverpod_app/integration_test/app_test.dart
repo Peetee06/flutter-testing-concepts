d345518ee365dd070640250f6d9f87387d096f3e
Peter Trost <p.trost93@gmail.com>
2025-12-22 19:04:46 +0100

    test: add challenges robot and progress flow integration test

--- riverpod_app/integration_test/app_test.dart
+++ riverpod_app/integration_test/app_test.dart
@@ -1,17 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:riverpod_app/app/view/app.dart';
 import 'package:riverpod_app/data/services/cache_manager_provider.dart';
 import 'package:riverpod_app/features/challenges/view/challenges_view.dart';
-import 'package:riverpod_app/features/challenges/view/widgets/challenges_list.dart';
 import 'package:riverpod_app/features/concept/view/concept_view.dart';
-import 'package:riverpod_app/features/concept/view/widgets/sections_view.dart';
 import 'package:riverpod_app/features/concepts/view/concepts_view.dart';
 
+import './challenges_robot.dart';
 import './mocks.mocks.dart';
 
 void main() {
@@ -32,36 +28,59 @@ void main() {
 
   group('App Integration Test', () {
     testWidgets('Concept flow', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        ProviderScope(
-          overrides: [
-            cacheManagerProvider.overrideWithValue(cacheManager),
-          ],
-          child: const App(),
-        ),
+      final robot = ChallengesRobot(
+        tester,
+        overrides: [cacheManagerProvider.overrideWithValue(cacheManager)],
       );
-      await tester.pumpAndSettle();
+      await robot.pumpApp();
 
       expect(find.byType(ConceptsView), findsOneWidget);
 
-      await tester.tap(find.byType(ListTile).first);
-      await tester.pumpAndSettle();
+      await robot.tapFirstConcept();
 
       expect(find.byType(ConceptView), findsOneWidget);
 
-      await tester.tap(find.byKey(SectionsView.forwardButtonKey));
-      await tester.pumpAndSettle();
-      await tester.tap(find.byKey(SectionsView.forwardButtonKey));
-      await tester.pumpAndSettle();
-      await tester.tap(find.byKey(SectionsView.forwardButtonKey));
-      await tester.pumpAndSettle();
+      await robot.navigateThroughSectionsToChallenges();
 
       expect(find.byType(ChallengesView), findsOneWidget);
 
-      await tester.tap(find.byKey(ChallengesList.doneButtonKey));
-      await tester.pumpAndSettle();
+      await robot.tapDoneButton();
 
       expect(find.byType(ConceptsView), findsOneWidget);
+    });
+
+    testWidgets('Challenge progress flow', (WidgetTester tester) async {
+      final robot = ChallengesRobot(
+        tester,
+        overrides: [cacheManagerProvider.overrideWithValue(cacheManager)],
+      );
+      await robot.pumpApp();
+      await robot.tapFirstConcept();
+      await robot.navigateThroughSectionsToChallenges();
+
+      expect(find.byType(ChallengesView), findsOneWidget);
+      expect(robot.isSubmitButtonEnabled, isFalse);
+
+      await robot.tapAnswerOption('Option 2 EN');
+
+      expect(robot.isSubmitButtonEnabled, isTrue);
+
+      await robot.tapSubmitButton();
+
+      expect(robot.findIncorrectText, findsOneWidget);
+      expect(robot.findResetButton, findsOneWidget);
+
+      await robot.tapResetButton();
+
+      expect(robot.findIncorrectText, findsNothing);
+      expect(robot.findSubmitButton, findsOneWidget);
+
+      await robot.tapAnswerOption('Option 1 EN');
+      await robot.tapAnswerOption('Option 2 EN');
+      await robot.tapAnswerOption('Option 3 EN');
+      await robot.tapSubmitButton();
+
+      expect(robot.findCorrectText, findsOneWidget);
     });
   });
 }
---
+++ riverpod_app/integration_test/challenges_robot.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_app/app/view/app.dart';
+import 'package:riverpod_app/features/challenges/view/widgets/challenge_card.dart';
+import 'package:riverpod_app/features/challenges/view/widgets/challenges_list.dart';
+import 'package:riverpod_app/features/concept/view/widgets/sections_view.dart';
+
+class ChallengesRobot {
+  ChallengesRobot(this.tester, {this.overrides = const []});
+
+  final WidgetTester tester;
+  final List<Override> overrides;
+
+  Finder get _firstChallengeCard => find.byType(ChallengeCard).first;
+
+  Finder _findInFirstCard(Finder matching) => find.descendant(
+        of: _firstChallengeCard,
+        matching: matching,
+      );
+
+  Finder get findSubmitButton =>
+      _findInFirstCard(find.byKey(ChallengeCard.submitButtonKey));
+
+  Finder get findResetButton =>
+      _findInFirstCard(find.byKey(ChallengeCard.resetButtonKey));
+
+  Finder get findCorrectText => _findInFirstCard(find.text('Correct!'));
+
+  Finder get findIncorrectText => _findInFirstCard(find.text('Incorrect'));
+
+  bool get isSubmitButtonEnabled {
+    final button = tester.widget<FilledButton>(findSubmitButton);
+    return button.onPressed != null;
+  }
+
+  Future<void> pumpApp() async {
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: overrides,
+        child: const App(),
+      ),
+    );
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> tapFirstConcept() async {
+    await tester.tap(find.byType(ListTile).first);
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> navigateThroughSectionsToChallenges() async {
+    await tester.tap(find.byKey(SectionsView.forwardButtonKey));
+    await tester.pumpAndSettle();
+    await tester.tap(find.byKey(SectionsView.forwardButtonKey));
+    await tester.pumpAndSettle();
+    await tester.tap(find.byKey(SectionsView.forwardButtonKey));
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> tapDoneButton() async {
+    await tester.tap(find.byKey(ChallengesList.doneButtonKey));
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> tapAnswerOption(String optionText) async {
+    await tester.tap(_findInFirstCard(find.text(optionText)));
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> tapSubmitButton() async {
+    await tester.tap(findSubmitButton);
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> tapResetButton() async {
+    await tester.tap(findResetButton);
+    await tester.pumpAndSettle();
+  }
+}
