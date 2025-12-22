94618e59ea53cd5dad597e5c3040e88b5596d947
Peter Trost <p.trost93@gmail.com>
2025-12-22 20:20:44 +0100

    test: add syntax error to test CI pipeline validation

--- common/lib/src/typedefs.dart
+++ common/lib/src/typedefs.dart
@@ -1,2 +1,5 @@
 /// A map of language codes to localized strings.
 typedef Localizations = Map<String, String>;
+
+/// This is a syntax error for CI testing
+const syntaxError = // missing value intentionally
